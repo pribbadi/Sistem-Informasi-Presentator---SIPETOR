4dee5e4c629218b3c387494d42bbca10134b7ceb
pribbadi <pribbadi@users.noreply.github.com>
2018-10-05 07:03:15 +0700

    script sipetor php7

---
+++ 4. Implementasi/PHP7/koneksi.php
@@ -0,0 +1,7 @@
+<?php
+$host = "localhost";
+$username = "root";
+$password = "";
+$databasename = "sipetor";
+$link = mysqli_connect($host, $username, $password, $databasename) or die ("koneksi gagal");
+?>

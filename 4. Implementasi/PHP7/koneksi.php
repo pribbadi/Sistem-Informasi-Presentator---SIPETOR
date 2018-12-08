0b58b4acbe9e8c3acf3763073374f0bb70a03a54
pribbadi <pribbadi@users.noreply.github.com>
2018-12-08 10:20:32 +0700

    update aplikasi sipetor
    beberapa error penyesuaian PHP5 ke PHP7 InsyaAllah Clear

--- 4. Implementasi/PHP7/koneksi.php
+++
@@ -1,7 +0,0 @@
-<?php
-$host = "localhost";
-$username = "root";
-$password = "";
-$databasename = "sipetor";
-$link = mysqli_connect($host, $username, $password, $databasename) or die ("koneksi gagal");
-?>
